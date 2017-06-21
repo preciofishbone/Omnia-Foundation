20c0bf55d79fe0095f4ba474a7d3566811da96ac
Andreas Ekdahl <Andreas.Ekdahl@preciofishbone.se>
2017-06-21 15:46:22 +0200

    created project templates structure

---
+++ src/tooling/1.0.5421/project-templates/testhost/Program.cs
@@ -0,0 +1,19 @@
+﻿using Omnia.Foundation.Jobs.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtPkgTestHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var projectPath = "..\\..\\..\\$safeprojectname$";
+            //var spUrl = "https://yoursite.sharepoint.com";
+            //var host = new DevExtensionHost(projectPath, spUrl);
+        }
+    }
+}
---
+++ src/tooling/1.0.5421/project-templates/webapi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExtPkgApi.Global" Language="C#" %>
