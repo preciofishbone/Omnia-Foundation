10941ff256486a7b1c8cf1fca4f18ee22da48442
Andreas Ekdahl <Andreas.Ekdahl@preciofishbone.se>
2017-06-29 23:31:47 +0200

    Added some more item templates

---
+++ src/tooling/1.0.5421/item-templates/job-queue/Job.cs
@@ -0,0 +1,33 @@
+﻿using Omnia.Foundation.Extensibility.Jobs;
+using Omnia.Foundation.Extensibility.Core;
+using Omnia.Foundation.Extensibility;
+using Omnia.Foundation.Models.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    [JobDefinition(
+       id: "$guid1$",
+       name: "$fileinputname$",
+       description: ""       
+    )]
+    public class $fileinputname$ : OmniaJob
+    {
+        // TODO: replace the type of queueMessage with the correct type
+        public void $fileinputname$Queue([QueueTrigger("$fileinputname$")] object queueMessage)
+        {
+            try
+            {
+                // Your job code here
+            }
+            catch (Exception ex)
+            {
+                WorkWith().Logging().AddLog("$fileinputname$Queue", ex.Message, DefaultLogTypes.Error, ex);
+            }
+        }
+    }
+}
---
+++ src/tooling/1.0.5421/item-templates/job-timer/Job.cs
@@ -0,0 +1,32 @@
+﻿using Omnia.Foundation.Extensibility.Jobs;
+using Omnia.Foundation.Extensibility.Core;
+using Omnia.Foundation.Extensibility;
+using Omnia.Foundation.Models.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    [JobDefinition(
+       id: "$guid1$",
+       name: "$fileinputname$",
+       description: ""       
+    )]
+    public class $fileinputname$ : OmniaJob
+    {
+        public void $fileinputname$Timer([TimerTrigger("01:00:00")] TimerInfo timerInfo)
+        {
+            try
+            {
+                // Your job code here
+            }
+            catch (Exception ex)
+            {
+                WorkWith().Logging().AddLog("$fileinputname$Timer", ex.Message, DefaultLogTypes.Error, ex);
+            }
+        }
+    }
+}
