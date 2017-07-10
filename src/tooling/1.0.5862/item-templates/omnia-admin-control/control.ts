3cb24d64fbe408bcd8525cdf581a62e0849e8a10
nhanle02021985 <nhanle02021985@gmail.com>
2017-07-10 08:30:13 +0200

    add admin settings template

---
+++ src/tooling/1.0.5862/item-templates/omnia-admin-control/control.ts
@@ -0,0 +1,32 @@
+import { OmniaAdminControl, OmniaExtensibilityAdminModule, LocalizePrefix, NavigationScope, PermissionScopes, LocalizationService } from '@omnia/foundation/extensibility';
+
+@OmniaAdminControl({
+    selector: '<YourComponentSelectorHere>',
+    imports: [OmniaExtensibilityAdminModule],
+    navigation: {
+        scope: NavigationScope.tenant,      
+        title: "", // TODO: Put the localization of your settings page display name here
+        state: "$fileinputname$state",
+        url: "$fileinputname$",
+        iconClass: "fa-bars",
+        authorizedRoles: [
+            {
+                name: '$fileinputname$Settings.Admin',
+                scope: PermissionScopes.tenant
+            }
+        ],
+        children: ['$fileinputname$permissionstate']
+    },
+    templateId:"", // TODO: Put the tenant resource id of '$fileinputname$.html' here
+})
+export class  $fileinputname$Settings implements OnInit {
+    constructor( @Inject(ViewContainerRef) private viewContainer: ViewContainerRef) {
+        this.init();
+    }
+
+    private init = () => {
+    }  
+
+    ngOnInit() {
+    }
+}
---
+++ src/tooling/1.0.5862/item-templates/omnia-admin-control/permission.ts
@@ -0,0 +1,48 @@
+import { Inject, AfterViewInit, OnInit, OnChanges } from '@angular/core';
+import { Security } from "@omnia/foundation/models";
+import {
+    OmniaAdminPermissionBase, OmniaExtensibilityAdminModule,
+    LocalizePrefix, Enums, PermissionScopes, PermissionRoles, TemplateId, AdminAuthorizationService
+} from '@omnia/foundation/extensibility';
+
+
+let authorizedRoles = [
+    {
+        name: PermissionRoles.OmniaAdmin,
+        scope: PermissionScopes.Tenant
+    }
+] 
+
+@OmniaAdminControl({
+    selector: '<YourComponentSelectorHere>',
+    navigation: {
+        title: "", // TODO: Put the localization of your settings page display name here
+        state: "$fileinputname$permissionstate'",
+        url: "permission",
+        iconClass: "fa-key",
+        authorizedRoles: authorizedRoles,
+    },
+    templateId:"", // TODO: Put the tenant resource id of '$fileinputname$Permission.html' here
+})
+export class $fileinputname$Permission extends OmniaAdminPermissionBase {
+    roleDefinitionList: Array<Security.IPermissionRoleDefinition> = [];
+
+    constructor(
+        @Inject(AdminAuthorizationService) public authorizationService: AdminAuthorizationService) {
+        super(authorizationService, authorizedRoles);
+        if (this._isAuthorized)
+            this.init();
+    }
+
+    private init = () => {
+        this.roleDefinitionList = [
+            {
+                name: "$fileinputname$Settings.Admin",
+                scope: PermissionScopes.Tenant,
+                label: "$fileinputname$Settings.Permissions.Admin.Label",
+                description: "$fileinputname$Settings.Permissions.Admin.Description"
+            }
+        ];
+    }
+}
+
